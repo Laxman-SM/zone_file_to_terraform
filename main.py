70c4878d1391f00692d1112d7498e3d2b44aabd5
Max.Weaver <max.weaver@infectiousmedia.com>
2016-11-11 18:26:44 +0000

    cleaning up comments

--- main.py
+++ main.py
@@ -7,31 +7,19 @@ import pprint
 with open('zone_files/inmz.net.zonefile.txt') as f:
     zone_file = f.readlines()
 
-empty_file = open('inmz.tf', 'w')
+inmz_zone_final = open('inmz.tf', 'w')
 
+# empty objects for output
 data = []
 json_object = []
+
+# this is a constant throughout terraform file
 zone_id = "${aws_route53_zone.inmz.zone_id}"
 
 for line in zone_file:
     stripped = re.sub(' +', ' ', line)
     words = stripped.split(' ')
     data.append(words)
-    #
-    # if len(words) > 1:
-    #     key = words[FIRST_ELEMENT]
-    #     del words[FIRST_ELEMENT]
-    #     dict[key] = words
-    # elif len(words) == 1:
-    #     dict[words[FIRST_ELEMENT]] = 'null'
-#
-# list = dict['drone']
-#
-# name = list[0]
-# type = list[1]
-# ttl = list[2]
-
-# print(name, type, ttl)
 
 for each_list in data:
     terrarecord = {
@@ -51,4 +39,4 @@ for each_list in data:
     json_object.append(terrarecord)
 
 for item in json_object:
-    empty_file.write('%s' % json_object)
+    inmz_zone_final.write('%s' % json_object)
