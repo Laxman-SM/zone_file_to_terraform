194aea4da1bb2f2c4a4c520a701cdd4a26840b1d
max-l-weaver <max.weaver@infectiousmedia.com>
2016-11-12 22:11:14 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 import os
-import re
 import json
-import io
+import re
 
 
-with open('zone_files/inmz.net.zonefile.txt') as f:
+with open('../zone_files/inmz.net.stripped.zonefile.txt') as f:
     zone_file = f.readlines()
 
 # .tf file should be moved to dedicated tf dir
-inmz_zone_final = open('inmz.tf', 'w')
+inmz_zone_final = open('../roles/route53/inmz.tf', 'w')
 
 # empty objects for output
 data = []
-json_object = []
+json_object = {'resource': {'aws_route53_record': None}}
+terrarecord = {}
 
 # this is a constant throughout terraform file
 zone_id = "${aws_route53_zone.inmz.zone_id}"
+url = '.inmz.net.'
 
+# remove whitespace and create data list
 for line in zone_file:
     stripped = re.sub(' +', ' ', line)
     words = stripped.split(' ')
     data.append(words)
 
+# create our terraform file here
 for each_list in data:
-    terrarecord = {
-        "resource": {
-            "aws_route53_record": {
-                each_list[0]:
-                    {
-                        "zone_id": zone_id,
-                        "name": each_list[0],
-                        "type": each_list[2],
-                        "ttl": each_list[1],
-                        "records": [each_list[3].replace('\n', '')]
-                    }
-            }
-        }
+    terrarecord[each_list[0].replace('.', '')] = {
+        "zone_id": zone_id,
+        "name": each_list[0] + url, # not sure if I need to do this
+        "type": each_list[2],
+        "ttl": each_list[1],
+        "records": [each_list[3].replace('\n', '')]
     }
-    json_object.append(terrarecord)
 
+json_object['resource']['aws_route53_record'] = terrarecord
+
+# write to file
 for item in json_object:
     inmz_zone_final.write('%s' % json.dumps(json_object))
 
+inmz_zone_final.close()
+f.close()
